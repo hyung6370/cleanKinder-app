c53cf67048216d40ca8f05b77678b435c21e1960
emptyhead <korea6370@yu.ac.kr>
2023-08-10 23:20:55 +0900

    Initial Commit

---
+++ OurKids/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  OurKids
+//
+//  Created by KIM Hyung Jun on 2023/08/10.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lottieView: LottieAnimationView!
+    @IBOutlet weak var kinderButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lottieAnimation()
+        configureUI()
+    }
+
+    
+    func lottieAnimation() {
+        let animationView: LottieAnimationView = .init(name: "homeKid")
+        animationView.contentMode = .scaleAspectFit
+        lottieView.addSubview(animationView)
+        
+        animationView.frame = lottieView.bounds
+        animationView.loopMode = .loop
+        animationView.play()
+    }
+    
+    func configureUI() {
+        kinderButton.layer.cornerRadius = 20
+        kinderButton.clipsToBounds = true
+    }
+
+    @IBAction func kinderButtonTapped(_ sender: UIButton) {
+        
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-OurKids/Pods-OurKids-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OurKids : NSObject
+@end
+@implementation PodsDummy_Pods_OurKids
+@end
